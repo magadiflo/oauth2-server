d6caf6b74a43e0a5d1c5f38eec84064285f208d6
Martín <magadiflo@gmail.com>
2022-11-07 18:11:45 -0500

    Creando la interfaz Feign Client

--- src/main/java/com/magadiflo/oauth2/server/Oauth2ServerApplication.java
+++ src/main/java/com/magadiflo/oauth2/server/Oauth2ServerApplication.java
@@ -3,7 +3,9 @@ package com.magadiflo.oauth2.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // Habilitamos el uso del cliente feign. Para poder comunicar este microservicio con otro (similar al restTesmplate)
 @EnableEurekaClient
 @SpringBootApplication
 public class Oauth2ServerApplication {
---
+++ src/main/java/com/magadiflo/oauth2/server/clients/IUserFeignClient.java
@@ -0,0 +1,18 @@
+package com.magadiflo.oauth2.server.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.magadiflo.commons.users.models.entity.User;
+
+//@FeignClient, para decirle que esta interfaz sea un cliente Feign
+//name, nombre del microservicio con el que nos queremos comunicar
+
+@FeignClient(name = "microservice-users")
+public interface IUserFeignClient {
+
+	@GetMapping(path = "/users/search/find-username")
+	public User findByUsername(@RequestParam("user") String username);
+
+}
