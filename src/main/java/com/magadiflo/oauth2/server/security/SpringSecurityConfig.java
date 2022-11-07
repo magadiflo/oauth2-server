289d18b2d3cca114e71e81a59747206585cfd8d3
Martín <magadiflo@gmail.com>
2022-11-07 18:53:52 -0500

    Añadiendo la clase SpringSecurityConfig y registrando el UserDetailsService

---
+++ src/main/java/com/magadiflo/oauth2/server/security/SpringSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.magadiflo.oauth2.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final UserDetailsService userService;
+	
+	public SpringSecurityConfig(UserDetailsService userService) {
+		this.userService = userService;		
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
