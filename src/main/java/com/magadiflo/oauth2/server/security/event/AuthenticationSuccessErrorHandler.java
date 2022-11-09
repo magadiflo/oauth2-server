e415639fac904b30a818e3a097f8a3149a64d249
Martín <magadiflo@gmail.com>
2022-11-08 19:39:17 -0500

    Implementando evento tres intentos en el login

--- src/main/java/com/magadiflo/oauth2/server/security/event/AuthenticationSuccessErrorHandler.java
+++ src/main/java/com/magadiflo/oauth2/server/security/event/AuthenticationSuccessErrorHandler.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import com.magadiflo.commons.users.models.entity.User;
 import com.magadiflo.oauth2.server.services.IUserService;
+
+import feign.FeignException;
 
 @Component
 public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
@@ -40,13 +43,42 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		}
 
 		UserDetails user = (UserDetails) authentication.getPrincipal();
-
 		LOGGER.info("Success login: {}", user.getUsername());
+		
+		
+		// Como se ha autenticado correctamente, verificamos si tiene registrado algún intento fallido para poder actualizarlo y setearle a 0
+		User authenticatedUser = this.userService.findByUsername(authentication.getName());
+		if(authenticatedUser.getAttempts() != null && authenticatedUser.getAttempts() > 0) {
+			authenticatedUser.setAttempts(0);
+			this.userService.update(authenticatedUser, authenticatedUser.getId());
+		}
 	}
 
+	// Este método saltará cuando la contraseña sea ingresada de manera incorrecta, es decir, la persona ingresa
+	// el username correctamente, pero la contraseña no.
 	@Override
 	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
 		LOGGER.error("Error en el login: {}", exception.getMessage());
+		
+		try {
+			User user = this.userService.findByUsername(authentication.getName());
+			if(user.getAttempts() == null) {
+				user.setAttempts(0);				
+			}
+			
+			user.setAttempts(user.getAttempts() + 1);
+			LOGGER.info("Current attempts: {}", user.getAttempts());
+			
+			if(user.getAttempts() >= 3) {
+				LOGGER.error("User has exceeded the maximum attempts", user.getUsername());
+				user.setEnabled(false);
+			}
+			
+			this.userService.update(user, user.getId());
+		} catch(FeignException e) {
+			LOGGER.error("El usuario {} no existe en el sistema", authentication.getName());			
+		}
+		
 	}
 
 }
